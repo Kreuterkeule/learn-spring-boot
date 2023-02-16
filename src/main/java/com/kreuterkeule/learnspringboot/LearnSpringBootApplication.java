48700f6563c195340c231b9caf44c260c64217ef
kreuterkeule <moritz.siefke@protonmail.com>
2023-02-16 15:06:10 +0100

    commit via intellij

--- src/main/java/com/kreuterkeule/learnspringboot/LearnSpringBootApplication.java
+++ src/main/java/com/kreuterkeule/learnspringboot/LearnSpringBootApplication.java
@@ -1,21 +1,33 @@
 package com.kreuterkeule.learnspringboot;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.sql.SQLOutput;
 
 @Configuration
 @SpringBootApplication
 public class LearnSpringBootApplication extends SpringBootServletInitializer {
+
+	private static final Logger logger = LoggerFactory.getLogger(LearnSpringBootApplication.class);
 
 	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(LearnSpringBootApplication.class);
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringBootApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(LearnSpringBootApplication.class, args);
+		logger.info("{LOGGER}: Application [learn-spring-boot] started");
 	}
 
 }
--- src/main/java/com/kreuterkeule/learnspringboot/RestController.java
+++ src/main/java/com/kreuterkeule/learnspringboot/RestController.java
@@ -1,10 +1,11 @@
 package com.kreuterkeule.learnspringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController public class RestController {
 
     @GetMapping("/") public String getROOT() {
-        return "Welcome to ROOT / route";
+        return "Welcome to api ROOT /";
     }
 }
